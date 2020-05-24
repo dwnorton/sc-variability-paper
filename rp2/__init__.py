08580d9437d2494be6b42e6c30b83d4f251ede02
dwnorton <64930308+dwnorton@users.noreply.github.com>
2020-05-24 16:04:00 +0100

    Save statistics and regression model parameters to .csv files

--- rp2/__init__.py
+++ rp2/__init__.py
@@ -4,10 +4,15 @@ from pathlib import Path
 
 
 data_path = Path(__file__).parent.parent.joinpath("Data")
+output_path = Path(__file__).parent.parent.joinpath("Output")
 
 
 def get_data_path(*names):
     return data_path.joinpath(*names)
+
+
+def get_output_path(*names):
+    return output_path.joinpath(*names)
 
 
 def get_url_filename(url):
