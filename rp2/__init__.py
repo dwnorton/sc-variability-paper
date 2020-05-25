47f248e308d03b73e42d03073fe821f6cd67ec31
dwnorton <64930308+dwnorton@users.noreply.github.com>
2020-05-25 23:54:02 +0100

    Added a work-in-progress notebook to perform burst model fitting

--- rp2/__init__.py
+++ rp2/__init__.py
@@ -1,6 +1,10 @@
+import shutil
 import urllib.parse
 import urllib.request
+import contextlib
+import os
 from pathlib import Path
+
 
 data_path = Path(__file__).parent.parent.joinpath("Data")
 output_path = Path(__file__).parent.parent.joinpath("Output")
@@ -19,6 +23,13 @@ def get_scripts_path(*names):
     return scripts_path.joinpath(*names)
 
 
+def create_folder(path, create_clean=False):
+    path = Path(path)
+    if create_clean and path.exists():
+        shutil.rmtree(path)
+    path.mkdir(parents=True, exist_ok=True)
+
+
 def get_url_filename(url):
     return Path(urllib.parse.urlparse(url).path).name
 
@@ -27,7 +38,7 @@ def download_file(url, to_dir, rename_to=None):
     to_dir = Path(to_dir)
     filename = get_url_filename(url) if rename_to is None else rename_to
     dst = to_dir.joinpath(filename)
-    to_dir.mkdir(parents=True, exist_ok=True)
+    create_folder(to_dir)
     return urllib.request.urlretrieve(url, dst)[0]
 
 
@@ -39,3 +50,13 @@ def fetch_file(url, to_dir, rename_to=None):
         download_file(url, to_dir=to_dir, rename_to=rename_to)
 
     return filepath
+
+
+@contextlib.contextmanager
+def working_directory(path):
+    prev_wd = Path.cwd()
+    os.chdir(path)
+    try:
+        yield
+    finally:
+        os.chdir(prev_wd)
