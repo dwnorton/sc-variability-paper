de07226d8954ad6bd927fc15dde3dafeef548d91
dwnorton <64930308+dwnorton@users.noreply.github.com>
2020-06-08 13:47:04 +0100

    Moved project path variables and functions into a dedicated module (paths)

--- rp2/__init__.py
+++ rp2/__init__.py
@@ -6,23 +6,6 @@ import os
 from pathlib import Path
 
 
-data_path = Path(__file__).parent.parent.joinpath("Data")
-output_path = Path(__file__).parent.parent.joinpath("Output")
-scripts_path = Path(__file__).parent.parent.joinpath("Scripts")
-
-
-def get_data_path(*names):
-    return data_path.joinpath(*names)
-
-
-def get_output_path(*names):
-    return output_path.joinpath(*names)
-
-
-def get_scripts_path(*names):
-    return scripts_path.joinpath(*names)
-
-
 def create_folder(path, create_clean=False):
     path = Path(path)
     if create_clean and path.exists():
