e6d10c7f15f58e4b2e457b9dbe6a67c9796517ae
dwnorton <64930308+dwnorton@users.noreply.github.com>
2020-06-25 23:14:40 +0100

    Reworked txburst parameter fitting to use the gene list from Data_Processing and to calculate results only for conditions that don't already have any

--- rp2/notebooks.py
+++ rp2/notebooks.py
@@ -2,9 +2,33 @@ from rp2 import check_environment, create_folder
 from rp2.paths import get_output_path
 
 
+class NotebookDependencyError(Exception):
+    pass
+
+
+class NotebookDependency:
+    def __init__(self, name, data_path):
+        self._name = name
+        self._data_path = data_path
+
+    def access_path(self, *names):
+        path = self._data_path.joinpath(*names)
+        if not path.exists():
+            raise NotebookDependencyError(f'Content missing from "{self._name}" - ensure the notebook has been executed')
+        return path
+
+
 class NotebookEnvironment:
     def __init__(self, intermediate_path):
         self._intermediate_path = intermediate_path
+
+    def register_dependency(self, nb_name):
+        root_path = self._intermediate_path.parent
+        nb_intermediate_path = root_path.joinpath(nb_name)
+        if not nb_intermediate_path.is_dir():
+            raise NotebookDependencyError(f'Please execute notebook "{nb_name}" prior to running this one')
+
+        return NotebookDependency(nb_name, nb_intermediate_path)
 
     def get_intermediate_path(self, *names):
         return self._intermediate_path.joinpath(*names)
@@ -13,10 +37,7 @@ class NotebookEnvironment:
 def initialise_environment(nb_name):
     check_environment()
 
-    output_path = get_output_path()
-    create_folder(output_path)
-
-    intermediate_path = output_path.joinpath(".intermediate", nb_name)
+    intermediate_path = get_output_path(".intermediate", nb_name)
     create_folder(intermediate_path, create_clean=True)
 
     return NotebookEnvironment(intermediate_path)
