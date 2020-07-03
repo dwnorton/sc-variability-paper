16e71dfa617c49a062cc7e96d206f30a1d25121f
dwnorton <64930308+dwnorton@users.noreply.github.com>
2020-07-03 21:45:05 +0100

    Added a class to encapsulate (parameterised) filename construction

--- rp2/paths.py
+++ rp2/paths.py
@@ -1,4 +1,6 @@
+import os
 from pathlib import Path
+
 
 data_path = Path(__file__).parent.parent.joinpath("Data")
 output_path = Path(__file__).parent.parent.joinpath("Output")
@@ -21,6 +23,25 @@ def get_txburst_results_path(*names):
     return get_output_path("txburst", *names)
 
 
+class ParameterisedFilename(os.PathLike):
+    def __init__(self, ext=None):
+        self._filename = ""
+        self._ext = f".{ext}" or ""
+
+    def __fspath__(self):
+        return self._filename + self._ext
+
+    def append(self, text):
+        if self._filename:
+            self._filename += "_"
+        self._filename += text
+
+
 def get_txburst_results_csv_path(species, combined_replicates=False):
-    suffix = "_combined_replicates" if combined_replicates else ""
-    return get_txburst_results_path(f"{species}_umi{suffix}.csv")
+    filename = ParameterisedFilename(ext="csv")
+    filename.append(species)
+    filename.append("umi")
+    if combined_replicates:
+        filename.append("combined_replicates")
+
+    return get_txburst_results_path(filename)
