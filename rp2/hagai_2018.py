007e29a382bb10b813ee759313b0aef14787203b
dwnorton <64930308+dwnorton@users.noreply.github.com>
2020-07-03 17:04:50 +0100

    Renamed functions for calculating count statistics so they do not purport to be UMI-specific

--- rp2/hagai_2018.py
+++ rp2/hagai_2018.py
@@ -3,9 +3,10 @@ import functools
 import anndata
 import numpy as np
 import pandas as pd
+import scanpy
 from scipy import stats
 
-from rp2 import load_biomart_gene_symbols_df
+from rp2 import load_biomart_gene_symbols_df, create_normalised_adata
 from rp2.paths import get_data_path
 
 
@@ -21,45 +22,45 @@ def load_lps_responsive_genes(species="mouse", max_padj=0.01):
     return fc_divergence_df.loc[fc_divergence_df[f"{species}_padj"] < max_padj].gene
 
 
-def load_umi_count(species):
+def load_umi_counts(species):
     return anndata.read_h5ad(get_data_path("ArrayExpress", f"E-MTAB-6754.processed.2.{species}.h5ad"))
 
 
 def load_umi_counts_with_additional_annotation(species):
-    umi_count_ad = load_umi_count(species)
+    umi_counts_adata = load_umi_counts(species)
 
     gene_info_df = load_biomart_gene_symbols_df(species)
-    umi_count_ad.var["symbol"] = gene_info_df.loc[umi_count_ad.var_names, "symbol"]
+    umi_counts_adata.var["symbol"] = gene_info_df.loc[umi_counts_adata.var_names, "symbol"]
 
     lps_responsive_ids = load_lps_responsive_genes()
-    umi_count_ad.var["lps_responsive"] = umi_count_ad.var_names.isin(lps_responsive_ids)
+    umi_counts_adata.var["lps_responsive"] = umi_counts_adata.var_names.isin(lps_responsive_ids)
 
-    return umi_count_ad
+    return umi_counts_adata
 
 
-def calculate_umi_subset_condition_stats(umi_count_ad, obs_subset):
-    umi_subset = umi_count_ad[obs_subset.index, :]
-    umi_matrix = umi_subset.X.A
+def calculate_counts_subset_condition_stats(umi_count_ad, obs_subset):
+    counts_subset = umi_count_ad[obs_subset.index, :]
+    counts_matrix = counts_subset.X.A
     return pd.DataFrame(
-        index=pd.Index(umi_subset.var_names, name="gene"),
+        index=pd.Index(counts_subset.var_names, name="gene"),
         data={
             "n_barcodes": len(obs_subset),
-            "min": np.min(umi_matrix, axis=0),
-            "max": np.max(umi_matrix, axis=0),
-            "mean": np.mean(umi_matrix, axis=0),
-            "variance": np.var(umi_matrix, ddof=1, axis=0),
-            "std_dev": np.std(umi_matrix, ddof=1, axis=0),
-            "skew": stats.skew(umi_matrix, axis=0),
+            "min": np.min(counts_matrix, axis=0),
+            "max": np.max(counts_matrix, axis=0),
+            "mean": np.mean(counts_matrix, axis=0),
+            "variance": np.var(counts_matrix, ddof=1, axis=0),
+            "std_dev": np.std(counts_matrix, ddof=1, axis=0),
+            "skew": stats.skew(counts_matrix, axis=0),
         }
     )
 
 
-def calculate_umi_condition_stats(umi_count_ad, group_columns=None, sort_columns=None):
+def calculate_counts_condition_stats(counts_adata, group_columns=None, sort_columns=None):
     group_columns = group_columns or ["replicate", "treatment", "time_point"]
     sort_columns = sort_columns or ["gene", "replicate", "time_point", "treatment"]
 
-    stats_fun = functools.partial(calculate_umi_subset_condition_stats, umi_count_ad)
-    condition_stats_df = umi_count_ad.obs.groupby(group_columns).apply(stats_fun)
+    stats_fun = functools.partial(calculate_counts_subset_condition_stats, counts_adata)
+    condition_stats_df = counts_adata.obs.groupby(group_columns).apply(stats_fun)
     condition_stats_df = condition_stats_df.sort_values(sort_columns)
     condition_stats_df = condition_stats_df.reset_index()
 
