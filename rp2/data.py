f04f3e38bc34a941eba02d8a12681ae90960ea51
dwnorton <64930308+dwnorton@users.noreply.github.com>
2020-08-04 11:47:02 +0100

    Updated a couple of notebooks to incorporate "modified" txburst results (i.e. with manually calculated size and frequency values in place of the profile likelihood estimates)

--- rp2/data.py
+++ rp2/data.py
@@ -8,3 +8,13 @@ def load_txburst_results(species, index_columns, count_type):
     dtype_dict = {column: "category" for column in index_columns}
 
     return pd.read_csv(csv_path, dtype=dtype_dict)
+
+
+def load_modified_txburst_results(species, condition_columns, count_type, two_alleles=False):
+    df = load_txburst_results(species, condition_columns, count_type)
+    df = df.loc[df.bf_point.notna() & df.bs_point.notna()]
+
+    df["bf_point"] = (2 * df.k_on * df.k_off) / (df.k_on + df.k_off) if two_alleles else df.k_on
+    df["bs_point"] = df.k_syn / df.k_off
+
+    return df.drop(columns=["bf_lower", "bf_upper", "bs_lower", "bs_upper", "keep"])
