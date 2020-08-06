6cb61898157669b46921c7e8bf5d72f74a832e19
dwnorton <64930308+dwnorton@users.noreply.github.com>
2020-08-06 11:00:04 +0100

    Updated txburst parameter recalculation to calculate "bursty" or "general" frequency value

--- rp2/data.py
+++ rp2/data.py
@@ -10,11 +10,11 @@ def load_txburst_results(species, index_columns, count_type):
     return pd.read_csv(csv_path, dtype=dtype_dict)
 
 
-def load_modified_txburst_results(species, condition_columns, count_type, two_alleles=False):
+def load_and_recalculate_txburst_results(species, condition_columns, count_type, bursty=True):
     df = load_txburst_results(species, condition_columns, count_type)
     df = df.loc[df.keep]
 
-    df["bf_point"] = (2 * df.k_on * df.k_off) / (df.k_on + df.k_off) if two_alleles else df.k_on
+    df["bf_point"] = df.k_on if bursty else (df.k_on * df.k_off) / (df.k_on + df.k_off)
     df["bs_point"] = df.k_syn / df.k_off
 
     return df.drop(columns=["bf_lower", "bf_upper", "bs_lower", "bs_upper", "keep"])
