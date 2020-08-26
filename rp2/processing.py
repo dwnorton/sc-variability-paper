fa88d7303e8435dfddb8eca132c62eff4883764c
dwnorton <64930308+dwnorton@users.noreply.github.com>
2020-08-26 15:09:59 +0100

    Moved correlation calculation functions into processing module

--- rp2/processing.py
+++ rp2/processing.py
@@ -1,6 +1,6 @@
 import numpy as np
 import pandas as pd
-from scipy import linalg, spatial
+from scipy import linalg, spatial, stats
 
 
 def calculate_mahalanobis_distance(values):
@@ -17,3 +17,13 @@ def calculate_df_mahalanobis_distance(df, columns, name="distance"):
     distances = calculate_mahalanobis_distance(values)
 
     return pd.DataFrame(index=df.index, data={name: distances})
+
+
+def calculate_df_pearson_r(df, x_var, y_var):
+    cr, cp = stats.pearsonr(df[x_var], df[y_var])
+    return pd.Series(data={"r": cr, "r_pval": cp})
+
+
+def calculate_df_spearman_r(df, x_var, y_var):
+    sp_corr = stats.spearmanr(df[x_var], df[y_var])
+    return pd.Series(data={"r": sp_corr.correlation, "r_pval": sp_corr.pvalue})
