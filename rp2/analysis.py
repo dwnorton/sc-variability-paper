1c8ff4b943e2d771dea7b84072c239c437f9573f
dwnorton <64930308+dwnorton@users.noreply.github.com>
2020-09-10 12:04:32 +0100

    Restructured creation of orthologue Analysis objects

--- rp2/analysis.py
+++ rp2/analysis.py
@@ -2,25 +2,37 @@ from rp2 import hagai_2018, load_mouse_orthologues, load_biomart_gene_symbols_df
 
 
 class Analysis:
-    def __init__(self, mouse_gene_ids):
-        self._mouse_gene_ids = mouse_gene_ids
-        self._orthologues_df = None
+    def __init__(self, species, gene_ids, orthologues_df=None):
+        self._condition_columns = ["replicate", "treatment", "time_point"]
+        self._species = species
+        self._gene_ids = gene_ids
+        self._orthologues_df = orthologues_df
 
     @property
     def condition_columns(self):
-        return ["replicate", "treatment", "time_point"]
+        return self._condition_columns
 
     @property
-    def mouse_gene_ids(self):
-        return self._mouse_gene_ids
+    def species(self):
+        return self._species
 
-    def get_orthologue_gene_ids(self, species):
+    @property
+    def gene_ids(self):
+        return self._gene_ids
+
+    def create_orthologue_analysis(self, species):
+        if species == self._species:
+            return self
+
         if self._orthologues_df is None:
-            self._orthologues_df = load_mouse_orthologues()
-        return self._orthologues_df.loc[self._orthologues_df.index.isin(self._mouse_gene_ids), f"{species}_gene"]
+            self._orthologues_df = load_mouse_orthologues().reset_index()
+
+        gene_mask = self._orthologues_df[f"{self._species}_gene"].isin(self._gene_ids)
+        orthologue_gene_ids = self._orthologues_df.loc[gene_mask, f"{species}_gene"]
+        return Analysis(species, orthologue_gene_ids, self._orthologues_df)
 
 
-def create_default_analysis():
+def create_default_mouse_analysis():
     additional_gene_symbols = ["Il1b", "Tnf"]
 
     gene_info_df = load_biomart_gene_symbols_df("mouse")
@@ -29,4 +41,4 @@ def create_default_analysis():
     lps_responsive_genes = hagai_2018.load_lps_responsive_genes()
     analysis_gene_ids = sorted(set(additional_gens_ids).union(lps_responsive_genes))
 
-    return Analysis(analysis_gene_ids)
+    return Analysis("mouse", analysis_gene_ids)
