a2f2dd614073447ee2407f2d238bf9c58fdd31f5
dwnorton <64930308+dwnorton@users.noreply.github.com>
2020-09-19 22:01:05 +0100

    Added (robust) linear regression line of mean-variance relationship for each species

--- rp2/regression.py
+++ rp2/regression.py
@@ -1,6 +1,8 @@
 import numpy as np
+import pandas as pd
 from scipy import optimize
 from sklearn import metrics
+from statsmodels import api as sm
 
 
 def power_function(x, a, b, c):
@@ -35,3 +37,19 @@ def calculate_curve_fit(df, x_var, y_var, loss_function, f_scale):
         "c": c,
         "r2": r2,
     }
+
+
+def fit_robust_linear_trend(df, x_var, y_var):
+    x = sm.add_constant(df[x_var])
+    y = df[y_var]
+    model = sm.RLM(y, x, M=sm.robust.norms.HuberT(t=1.345))
+    rlm_results = model.fit()
+    results = {
+        "intercept": rlm_results.params[0],
+        "slope": rlm_results.params[1],
+        "intercept_pval": rlm_results.pvalues[0],
+        "slope_pval": rlm_results.pvalues[1],
+        "r2_unweighted": metrics.r2_score(y, rlm_results.fittedvalues),
+        "r2_weighted": metrics.r2_score(y, rlm_results.fittedvalues, sample_weight=rlm_results.weights),
+    }
+    return pd.Series(results)
